7b5d28f6614142ca88e6f92c207d74a9f879944c
obylo98 <obinnaandrewonyekwere@gmail.com>
2025-03-21 16:38:20 +0000

    updating get endpoints

--- server.js
+++ server.js
@@ -2,9 +2,19 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;  
 
+const mongodb = require('./data/database');
+
 app.use('/', require('./routes'));  
 
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+mongodb.initDb((err, mongodb) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    }
 });
+
+
+
